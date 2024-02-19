9980a640878a33766edded65839eca976a72e112
Vabele <valerio.belenchia@studenti.unicam.it>
2024-02-19 12:02:24 +0100

    Aggiunta repository generico

---
+++ GestioneListaDistribuzioneMultiUtenza.Models/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using GestioneListaDistribuzioneMultiUtenza.Models.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneListaDistribuzioneMultiUtenza.Models.Repositories
+{
+    public abstract class GenericRepository<T> where T : class
+    {
+        protected MyDbContext _ctx;
+        public GenericRepository(MyDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public void Aggiungi(T entity)
+        {
+            _ctx.Set<T>().Add(entity);
+            this.Save();
+        }
+
+        public T Ottieni(object id)
+        {
+            return _ctx.Set<T>().Find(id);
+        }
+
+        public void Elimina(object id)
+        {
+            var entity = Ottieni(id);
+            if(entity != null)
+            {
+                _ctx.Set<T>().Remove(entity);
+                _ctx.SaveChanges();
+            }
+        }
+
+        public void Save()
+        {
+            _ctx.SaveChanges();
+        }
+
+    }
+}
