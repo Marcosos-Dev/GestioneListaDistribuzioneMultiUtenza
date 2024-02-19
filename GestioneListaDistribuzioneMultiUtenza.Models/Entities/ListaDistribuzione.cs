2195a16a51913d64693e4036c2d0f2ab01467d82
Vabele <valerio.belenchia@studenti.unicam.it>
2024-02-19 11:28:03 +0100

    Aggiunte le entità

---
+++ GestioneListaDistribuzioneMultiUtenza.Models/Entities/ListaDistribuzione.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneListaDistribuzioneMultiUtenza.Models.Entities
+{
+    public class ListaDistribuzione
+    {
+        public int IdLista {  get; set; }
+        public string NomeLista { get; set; }
+
+        public ICollection<EmailDestinataria> EmailDestinatarie { get; set; } = null!;
+        public Utente UtenteProprietario { get; set; }
+
+
+    }
+}
