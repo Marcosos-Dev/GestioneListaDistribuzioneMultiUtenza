b865c0407dbc0d9ce4a580dae3fd09de550c2b26
Marcosos-Dev <marcobellilorenzo@gmail.com>
2024-02-29 11:50:06 +0100

    fix response email

--- GestioneListaDistribuzioneMultiUtenza.Application/Models/Responses/SendEmailToListResponse.cs
+++ GestioneListaDistribuzioneMultiUtenza.Application/Models/Responses/SendEmailToListResponse.cs
@@ -4,6 +4,6 @@ namespace GestioneListaDistribuzioneMultiUtenza.Application.Models.Responses
 {
     public class SendEmailToListResponse
     {
-        public List<EmailDestinatariDto> EmailDestinatari = new List<EmailDestinatariDto>();
+        public List<EmailDestinatariDto> EmailDestinatari { get; set; } = new List<EmailDestinatariDto>();
     }
 }
