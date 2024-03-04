3e4dc698e62a2c50df8035ab27d1bed75538a9a4
Marcosos-Dev <marcobellilorenzo@gmail.com>
2024-03-04 11:12:45 +0100

    fix delete validator

--- GestioneListaDistribuzioneMultiUtenza.Application/Validators/DeleteDestinatarioFromListValidator.cs
+++ GestioneListaDistribuzioneMultiUtenza.Application/Validators/DeleteDestinatarioFromListValidator.cs
@@ -13,10 +13,10 @@ namespace GestioneListaDistribuzioneMultiUtenza.Application.Validators
                 .NotEmpty()
                 .WithMessage("Il campo id lista è obbligatorio (vuoto)");
             RuleFor(r => r.email)
-                .NotEmpty()
-                .WithMessage("Il campo email è obbligatorio (nullo)")
                 .NotNull()
-                .WithMessage("Il campo email è obbligatorio (vuoto)")
+                .WithMessage("Il campo id lista è obbligatorio (nullo)")
+                .NotEmpty()
+                .WithMessage("Il campo id lista è obbligatorio (vuoto)")
                 .Matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
                 .WithMessage("email non valida");
         }
