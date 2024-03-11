ecbb1930a82835de5231290f7cf828c3a3136b88
Marcosos-Dev <marcobellilorenzo@gmail.com>
2024-03-11 10:14:46 +0100

    Rimozione password DTO utente

--- GestioneListaDistribuzioneMultiUtenza.Application/Models/Dtos/UtenteDto.cs
+++ GestioneListaDistribuzioneMultiUtenza.Application/Models/Dtos/UtenteDto.cs
@@ -10,12 +10,10 @@ namespace GestioneListaDistribuzioneMultiUtenza.Application.Models.Dtos
             Email = utente.Email;
             Nome = utente.Nome;
             Cognome = utente.Cognome;
-            Password = utente.Password;
         }
 
         public string Email { get; set; } = string.Empty;
         public string Nome { get; set; } = string.Empty;
         public string Cognome { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
     }
 }
